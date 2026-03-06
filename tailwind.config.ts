acbbfdbdee80429bb1279a6d4362f1b9fcd574f4
Roberttheone <38722012+Roberttheone@users.noreply.github.com>
2026-03-06 10:44:53 +1000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/app/**/*.{ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx}"],
   theme: {
     extend: {
       boxShadow: { soft: "0 10px 30px rgba(0,0,0,.25)" },
